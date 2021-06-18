fa7fa34e4b57ef7aff1ee17c6cb156fc336b4849
FrancoBuenoHolberton <3290@holbertonschool.comgit config -- global user.email 3290@holbertonschool.com>
2021-06-18 06:14:44 -0700

    4Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#inside <stdio.h>
+* main - that prints exactly
+*        "Programming is like building a multilingual puzzle
+*        followed by a new line.
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
