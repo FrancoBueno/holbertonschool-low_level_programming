869a3bcdf23e81d76eb382fd0863e372ba2e2892
FrancoBuenoHolberton <3290@holbertonschool.comgit config -- global user.email 3290@holbertonschool.com>
2021-06-18 07:01:05 -0700

    Init commit -5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,10 @@
-#inside <stdio.h>
+#include <stdio.h>
+/**
+ * main - that print exactly
+ *
+ *
+ **/
 int main(void)
-{ printf("with proper grammar, but the outcome is a piece of art,")
+{ printf("with proper grammar, but the outcome is a piece of art,\n");
 
    }
