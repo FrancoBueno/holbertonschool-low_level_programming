decb3be60b16c479dc69f616f9a987c6a9a2f4df
FrancoBuenoHolberton <3290@holbertonschool.comgit config -- global user.email 3290@holbertonschool.com>
2021-06-18 11:23:46 -0700

    0finish

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+/* betty style doc for function main goes there */
 /**
- * main - determine if it is positive or negative
- * Return success (0)
+ * main - Entry point
+ *
+ * Return Always success (0)
+ *
  */
 int main(void)
 {
