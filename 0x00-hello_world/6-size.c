cc47b0cb372aeb69327351b297cf93e904ad8641
FrancoBuenoHolberton <3290@holbertonschool.comgit config -- global user.email 3290@holbertonschool.com>
2021-06-18 07:28:16 -0700

    Init commit 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,9 +4,8 @@
  *
  * return Always "0"
  **/
-
+int main (void);
 {
-  int main (void);
   printf("Size of a char %lu byte(s)\n", sizeof (char));
   printf("Size of a int %lu byte(s)\n" , sizeof (int));
   printf("Size of a long int %lu byte(S)\n", sizeof (long int));
