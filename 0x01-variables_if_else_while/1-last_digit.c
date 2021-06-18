8b9237547da0fb60d4a6d7d6acdbc9c009dca03d
FrancoBuenoHolberton <3290@holbertonschool.comgit config -- global user.email 3290@holbertonschool.com>
2021-06-18 11:58:18 -0700

    Init commit -m 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,16 +12,16 @@
  */
 int main(void)
 {
-  int n;
-  short lastNumber;
-  srand(time(0));
-  n = rand() - RAND_MAX / 2;
-  lastNumber = (n % 10);
-    if (lastNumber == 0)
-      printf("Last digit of %d is %d and is 0\n",n, lastNumber);
-    else if (lastNumber > 5)
-      printf("Last digit of %d is %d and is greater than 5\n", n, lastNumber);
-    else
-      printf("Last digit of %d is %d and is less than 6 and not 0\n",n , lastNumber);
-  return (0);
+int n;
+short lastNumber;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+lastNumber = (n % 10);
+if (lastNumber == 0)
+printf("Last digit of %d is %d and is 0\n", n, lastNumber);
+else if (lastNumber > 5)
+printf("Last digit of %d is %d and is greater than 5\n", n, lastNumber);
+else
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastNumber);
+return (0);
 }
