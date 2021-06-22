5a1deec1effd8527464a94e332eabfdc0aa2623f
FrancoBuenoHolberton <3290@holbertonschool.comgit config -- global user.email 3290@holbertonschool.com>
2021-06-22 12:39:54 -0700

    Init commit 2F

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,7 +10,7 @@ void print_alphabet_x10(void)
 int loop;
 char print10 = 'a';
  
-for (loop = 0; loop <= 10 ; loop++)
+for (loop = 1; loop <= 10 ; loop++)
 {
 for (print10 = 'a'; print10 <= 'z'; print10++)
 {
