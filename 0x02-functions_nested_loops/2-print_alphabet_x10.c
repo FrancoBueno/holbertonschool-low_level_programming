9d2437573081a1ac89ad0243c19ae4cec39de6f8
FrancoBuenoHolberton <3290@holbertonschool.comgit config -- global user.email 3290@holbertonschool.com>
2021-06-22 12:38:35 -0700

    Init commit 2F

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ *print_alphabet_x10 - print alphabet x10 in loop
+ *
+ *Return: void
+ */
+void print_alphabet_x10(void)
+{
+int loop;
+char print10 = 'a';
+ 
+for (loop = 0; loop <= 10 ; loop++)
+{
+for (print10 = 'a'; print10 <= 'z'; print10++)
+{
+_putchar(print10);
+ }
+_putchar('\n');
+}
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,3 +1,4 @@
 /**_putchar Print on screen */
 int _putchar (char c);
 void print_alphabet(void);
+void print_alphabet_x10(void);
