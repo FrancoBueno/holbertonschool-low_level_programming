6b865551752a80bf2c52a693584ac731a04bb729
FrancoBuenoHolberton <3290@holbertonschool.comgit config -- global user.email 3290@holbertonschool.com>
2021-06-22 11:52:39 -0700

    init commit1F

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ *main - main block
+ *Return: Always 0 (success)
+ **/
+void print_alphabet(void)
+{
+char character;
+ for (character ='a'; character <= 'z'; character++)
+_putchar (character);
+_putchar('\n');
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,2 +1,3 @@
 /**_putchar Print on screen */
 int _putchar (char c);
+void print_alphabet(void);
