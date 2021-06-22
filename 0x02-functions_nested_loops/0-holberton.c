b1655a715b9305c6e2d749e14114915cf07dff2c
FrancoBuenoHolberton <3290@holbertonschool.comgit config -- global user.email 3290@holbertonschool.com>
2021-06-22 10:11:50 -0700

    Init commit0

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,6 +1,17 @@
 #include "holberton.h"
 /**
  * main - Entry Point
- * return success (0)
+ * return Always 0 (success)
  * Write a program that prints Holberton, followed by a new line.
  */
+int main(void)
+{
+char word[] = "Holberton\n";
+
+int index;
+
+ for (index = 0; index <= 9; index++)
+_putchar(word[index]);
+
+return(0);
+}
