e9cabc09ee79c5774f776da6f28b7706390623db
Franco Bueno <3290@holbertonschool.com>
2021-06-23 09:58:16 -0700

    Init 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,33 +1,27 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
+ * print_sign - function that prints the sign of a number
  *
- * Return: Always 0.
+ * @n: char ascii in int
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately
  */
-int main(void)
+int print_sign(int n)
 {
-int r;
-
-r = print_sign(98);
-_putchar(',');
-_putchar(' ');
-_putchar(r + '0');
-_putchar('\n');
-r = print_sign(0);
-_putchar(',');
-_putchar(' ');
-_putchar(r + '0');
-_putchar('\n');
-r = print_sign(0xff);
-_putchar(',');
-_putchar(' ');
-_putchar(r + '0');
-_putchar('\n');
-r = print_sign(-1);
-_putchar(',');
-_putchar(' ');
-_putchar(r + '0');
-_putchar('\n');
-return (0);
-}
+  if (n == 0)
+    {
+      _putchar('0');
+      return (0);
+    }
+  else if (n > 1)
+    {
+      _putchar('+');
+      return (1);
+    }
+  else
+    {
+      _putchar('-');
+      return (-1);
+  
