f7ab92b1f2a48192dbd3a084f3f0ca44fd19be89
Franco Bueno <3290@holbertonschool.com>
2021-06-24 10:40:05 -0700

    Init commit1

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,11 @@
 #include "holberton.h"
+
 /**
- *_isupper - Entry Point
- *Return: if is lower return 0 if is upper return 1
+ *_isupper - Entry point
+ *@c:charater
+ *Return: if is lower or upper return 1 else 0
  */
-int _isupper(int c);
+int _isupper(int c)
 {
 if (c >= 'A' && c <= 'Z')
 return (1);
