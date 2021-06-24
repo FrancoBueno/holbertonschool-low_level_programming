8990dcab211ec17804e68f01fee46ba4664066f7
Franco Bueno <3290@holbertonschool.com>
2021-06-24 11:22:46 -0700

    init commit2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,10 @@
+/**
+ *Description - This function is to multiply two numbers
+ *@a: Integer to multiply
+ *@b: integrer to multiply
+ *Return: (c * b)
+ */
+int mul(int a, int b)
+{  
+return (a * b);
+}
