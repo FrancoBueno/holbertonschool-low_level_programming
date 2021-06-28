bbe73a3b5859838c633cea15b717a0f8fbda8b8b
Franco Bueno <3290@holbertonschool.com>
2021-06-28 11:53:24 -0700

    Init 1 Push

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ *void - Write a function that swaps the values of two integers.
+ *Return: 0 (Success)
+ *@a: i´m pointer of type int
+ *@b: im pointer of type int
+ */
+void swap_int(int *a, int *b);
+{
+  int intercambio;
+  
+  intercambio = *a;
+  *a = *b
+    *b = intercambio;
+}
