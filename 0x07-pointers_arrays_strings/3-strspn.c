fbda78cd93ae0f6621e44ca2a35fe09cf343cb7c
root <3290@holbertonschool.com>
2021-07-05 10:35:55 -0700

    init betty fix

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,28 +9,28 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-  int a;
-  int b;
-  int c;
+int a;
+int b;
+int c;
 
-  a = 0;
-  c = 0;
+a = 0;
+c = 0;
 
-  while (s[a] != '\0')
-    {
-      b = 0;
-      while (accept[b] != '\0')
-	{
-	  if (s[a] == accept[b])
-	    {
-	      c++;
-	      break;
-	    }
-	  b++;
-	}
-      if (accept[b] == '\0')
-	break;
-      a++;
-    }
-  return (c);
+while (s[a] != '\0')
+{
+b = 0;
+while (accept[b] != '\0')
+{
+if (s[a] == accept[b])
+{
+c++;
+break;
+}
+b++;
+}
+if (accept[b] == '\0')
+break;
+a++;
+}
+return (c);
 }
