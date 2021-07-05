377e0eb326f5ab442d268350dc351c8af5cb04c8
root <3290@holbertonschool.com>
2021-07-05 10:21:27 -0700

    Init 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ *  _strspn - check the code for Holberton School students.
+ * @s: char
+ * @accept: char
+ * Return: Always 0.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+int i, j;
+ 
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; accept[j]; j++)
+{
+if (accept[j] == s[i])
+break;
+}
+if (!accept[j])
+break;
+}
+return (i);
+}
