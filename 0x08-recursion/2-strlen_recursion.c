10a42b1bcf74bbeedec13eafe24d5071f2e9ff5f
root <3290@holbertonschool.com>
2021-07-07 09:05:16 -0700

    init 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *_strlen_recursion - Entry point 
+ *Return: Always 0 (success)
+ *@s :string 
+ */
+int _strlen_recursion(char *s)
+{
+  int a = 0;
+if (*s == '\0')
+{
+return (0);
+}
+ a++;
+ a = _strlen_recursion(s + a) + 1;
+ return (a);
+}
