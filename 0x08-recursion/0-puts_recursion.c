966b31d57d9476f03ac3700167a336f6d6041708
root <3290@holbertonschool.com>
2021-07-07 06:29:20 -0700

    init 1

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *_puts_recursion- Entry point 
+ *Return always 0 (Success)
+ *@s:char  
+ */
+void _puts_recursion(char *s)
+{
+if(*s == '\0')
+_putchar ('\n');
+
+else
+{
+  _putchar(*s);
+  _puts_recursion(s + 1);
+ }
+}
