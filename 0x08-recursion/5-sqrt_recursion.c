9e0f493d18c86519fe29b70273ff1cd861b5ab7d
root <3290@holbertonschool.com>
2021-07-07 11:07:41 -0700

    init 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+/**
+ * rec_num - calcule racine
+ * @y : int
+ * @n: int
+ * Return: int (mean true).
+ */
+int rec_num(int y, int n)
+{
+if (y * y == n)
+{
+return (y);
+}
+if (y * y > n)
+return (-1);
+else
+y++;
+return (rec_num(y, n));
+}
+/**
+ *_sqrt_recursion - racine carre
+ * @n: int
+ * Return: int
+ */
+int _sqrt_recursion(int n)
+{
+int x;
+
+if (n == 0)
+{
+return (0);
+}
+else
+x = rec_num(1, n);
+return (x);
+}
