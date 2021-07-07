d40d8ed4f88357734a1ac9bd853002dc9c9db786
root <3290@holbertonschool.com>
2021-07-07 07:04:41 -0700

    init finish 1 2

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -3,12 +3,13 @@
 /**
  *_print_rev_recursion - Entry point
  *return: Always 0 (Success)
+ *@s:char
  */
 void _print_rev_recursion(char *s)
 {
 if (*s == '\0')
 _putchar('\n');
- return;
+ else
 _print_rev_recursion(s + 1);
 _putchar(*s);
 }
