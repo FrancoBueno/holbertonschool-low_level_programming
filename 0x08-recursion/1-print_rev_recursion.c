1bb084a03a9f0b2a51dfd33f642852f1a0631404
root <3290@holbertonschool.com>
2021-07-07 07:00:48 -0700

    Init 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *_print_rev_recursion - Entry point
+ *return: Always 0 (Success)
+ */
+void _print_rev_recursion(char *s)
+{
+if (*s == '\0')
+_putchar('\n');
+ return;
+_print_rev_recursion(s + 1);
+_putchar(*s);
+}
