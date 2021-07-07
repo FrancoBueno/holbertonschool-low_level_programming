de3a60d245817e2040077224a3e63fafe9c62c9d
root <3290@holbertonschool.com>
2021-07-07 10:48:36 -0700

    init commit 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * _pow_recursion - claculate the power of two integer
+ * @x : int.
+ * @y : int.
+ * Return: int (mean tru).
+ */
+int _pow_recursion(int x, int y)
+{
+int res;
+
+if (y < 0)
+{
+return (-1);
+}
+if (y >= 1)
+{
+res = x * _pow_recursion(x, y - 1);
+return (res);
+}
+else
+return (1);
+}
