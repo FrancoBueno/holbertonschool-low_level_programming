6f85ae6c6dfbe2364c1a85d18e4727e3ba4a193e
root <3290@holbertonschool.com>
2021-07-09 09:28:40 -0700

    init commit 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,16 @@
 #include "holberton.h"
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
- *main - Entry point
- * Description: print a number , followed by a new line  
- *Return success 0
+ * main - Entry point
+ * Description : print a number , followed by a new line  
+ * Return success 0
+ * @argv: array
+ * @argc: array
  */
 int main(int argc, char *argv[])
 {
 (void) *argv;
 printf("%d\n", argc -1);
-return 0;
+return argc;
 }
