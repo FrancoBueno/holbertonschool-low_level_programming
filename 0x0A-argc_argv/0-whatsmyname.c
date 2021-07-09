b72aa75527aaeac4a7284be3445832975ea1ba76
root <3290@holbertonschool.com>
2021-07-09 07:04:38 -0700

    init commit 0fix

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print the name of program
+ * Description: if rename the program, it will print the name
+ * without compiling
+ * Return: the name, EXIT
+ */
+
+int main(int argc, char *argv[])
+{
+  (void) argc;
+  printf("%s,\n", argv[0]);
+  return 0;
+}
