7ab04848a5ca4bdc90288b0cc2a2b7076cdc1a9e
root <3290@holbertonschool.com>
2021-07-13 16:16:55 -0700

    init commit1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - a copy of the string given as a parameter
+ * Description: using malloc
+ * @str: the string to copy
+ * Return: a pointer to a newly allocated space in memory
+ */
+
+char *_strdup(char *str)
+{
+char *p = NULL;
+unsigned int i = 0, length = 0;
+
+if (str)
+{
+for (length = 0; *(str + length) != '\0'; length++)
+{
+/* size of string */
+}
+p = malloc(sizeof(char) * length + 1);
+if (p)
+{
+for (i = 0; i < length; i++)
+{
+p[i] = str[i];
+}
+p[length] = '\0';
+}
+}
+return (p);
+}
