d7b86481d10eaf2ada88bbc54c37f64484ccd49f
root <3290@holbertonschool.com>
2021-07-15 12:27:22 -0700

    init commit 2

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,42 +1,42 @@
 #include "holberton.h"
 #include <stdlib.h>
+
 /**
-* string_nconcat - entry point
-* @s1: string 1
-* @s2: string 2
-* @n: bytes .
-* Return: Nothing.
-*/
+ * string_nconcat - Concatenates two strings
+ * @s1: string 1.
+ * @s2: string 2.
+ * @n: The maximum number of bytes 
+ *
+ * Return: NULL fail, else pointer to malloc memory
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int f, l, k;
-	char *p;
+char *p;
+unsigned int len = n, i;
 
 if (s1 == NULL)
 s1 = "";
 
 if (s2 == NULL)
 s2 = "";
-f = 0;
-while (s1[f])
-f++;
 
-p = malloc(sizeof(*p) * f + n + 1);
+for (i = 0; s1[i]; i++)
+len++;
+
+p = malloc(sizeof(char) * (len + 1));
 
 if (p == NULL)
 return (NULL);
 
-for (l = 0, k = 0; l < (l + n); l++)
-{
-if (l < f)
-{
-p[l] = s1[l];
-}
-else
-{
-p[l] = s2[k++];
-}
-}
-p[l] = '\0';
+len = 0;
+
+for (i = 0; s1[i]; i++)
+p[len++] = s1[i];
+
+for (i = 0; s2[i] && i < n; i++)
+p[len++] = s2[i];
+
+p[len] = '\0';
+
 return (p);
 }
