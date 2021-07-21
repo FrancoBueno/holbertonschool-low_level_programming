ef91f491b9fbbfe7aa85574c8e3a408ea9922af8
root <3290@holbertonschool.com>
2021-07-21 09:15:37 -0700

    init commit 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include "function_pointers.h"
+
 /**
- *
+ *print_name - function of print name of the char
+ *@name: array de nombre
+ *@f: puntero de name
  */
 void print_name(char *name, void (*f)(char *))
 {
-  if ( name == NULL || f == NULL)
-    return;
-  f(name);
-
+if (name == NULL || f == NULL)
+return;
+f(name);
 }
