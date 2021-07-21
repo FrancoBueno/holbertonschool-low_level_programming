de27a2b1dedd6c690abbc1a74bc5262e323542c0
root <3290@holbertonschool.com>
2021-07-21 10:46:23 -0700

    init commit -m

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+/**
+ *int_index: index picado pa 
+ *@size: Tamaño del array
+ *@cmp: funcion del puntero
+ *return: return the index
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+if (size < 1 || array == NULL || cmp == NULL)
+{
+return (-1);
+}
+if (array && cmp)
+for (i = 0; i < size; i++)
+{
+if(cmp(array[i]))
+return (i);
+}
+return (-1);
+}
