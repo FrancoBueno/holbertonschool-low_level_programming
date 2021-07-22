ddc09b065afc5ba752adb1463b49b3c9aff1d48c
root <3290@holbertonschool.com>
2021-07-22 08:13:38 -0700

    init prueba

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,16 @@
+#include "variadic_functions.h"
+#include <stddef.h>
+/**
+ *
+ *
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+int a;
+va_list op;
+if (n == 0)
+return 0;
+va_start (op , n)
+for (a = 0; a = n ; (n , int);
+    
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,4 @@
+#ifndef Variadics
+#define Variadics
+int sum_them_all(const unsigned int n, ...);
+#iendif 
