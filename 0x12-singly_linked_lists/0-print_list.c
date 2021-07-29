05db046bf6cb636b033f88d15e5c58d764bf6f87
root <3290@holbertonschool.com>
2021-07-29 12:59:07 -0700

    betty betty

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,17 +9,17 @@
 
 size_t print_list(const list_t *h)
 {
-  int i = 0;
+	int i = 0;
 
 
-  while (h != NULL)
-    {
-      if (h->str != NULL)
-	printf("[%d] %s\n", h->len, h->str);
-      else
-	printf("[0] (nil)\n");
-      i++;
-      h = h->next;
-    }
-  return (i);
+	while (h != NULL)
+	{
+		if (h->str != NULL)
+			printf("[%d] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
+		i++;
+		h = h->next;
+	}
+	return (i);
 }
