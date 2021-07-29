563ea7b3c4598f70e463a08458b62713f6a42022
root <3290@holbertonschool.com>
2021-07-29 12:54:29 -0700

    1 y 2

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_list - print the element in the screen
+ * @h: headd
+ *
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+  int i = 0;
+
+
+  while (h != NULL)
+    {
+      if (h->str != NULL)
+	printf("[%d] %s\n", h->len, h->str);
+      else
+	printf("[0] (nil)\n");
+      i++;
+      h = h->next;
+    }
+  return (i);
+}
---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,16 @@
+#include "list.h"
+#include <stdio.h>
+/**
+ *
+ */
+size_t list_len(const list_t *h)
+{
+int a = 0;
+
+while (h != NULL)
+{
+a++;
+h = h->next;
+}
+return (a);
+}
