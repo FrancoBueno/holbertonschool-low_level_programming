d99cc967c5a8b24ddc0ae3dd1ce6e1b14bb98d75
root <3290@holbertonschool.com>
2021-07-29 13:10:58 -0700

    list 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,7 +1,9 @@
 #include "lists.h"
 #include <stdio.h>
 /**
- *
+ *list_len - Return the number of elements of list_t
+ *@h: variable pointer to struct
+ *Return: number of elements in linked list
  */
 size_t list_len(const list_t *h)
 {
