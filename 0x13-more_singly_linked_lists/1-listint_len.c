3fe6a795b6be5c84672d04eb45801c5c01cdda56
root <3290@holbertonschool.com>
2021-08-02 12:02:42 -0700

    3tree finish

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ *listint_len - print amount of elemnts
+ *Return: amount de elements (f)
+ *@h: Header
+ */
+size_t listint_len(const listint_t *h)
+{
+	int f = 0;
+
+	while (!h = NULL)
+		f++;
+		h = h->next;
+	}
+	return (f);
+}
