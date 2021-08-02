c00b4b285bcbd52ca4004fb6c9ac9a29f1b0507c
root <3290@holbertonschool.com>
2021-08-02 12:07:08 -0700

    betty

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -2,17 +2,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- *
+ *add_nodeint - function that print a list
+ *@head: header of the list
+ *Return: always sucess (0)
+ *@n: pointer save
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *newnode;
-	if(head == NULL)
+
+	if (head == NULL)
 		return (NULL);
 	newnode = malloc(sizeof(listint_t));
 		if (newnode == NULL)
 			return (NULL);
-	newnode->n = n; 
+	newnode->n = n;
 	newnode->next = *head;
 	*head = newnode;
 	return (*head);
