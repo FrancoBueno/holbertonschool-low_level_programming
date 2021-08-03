9c8c8777491dd9c9026ae04566f13ad810245016
root <3290@holbertonschool.com>
2021-08-03 10:26:04 -0700

    5?

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_listint2 - free list two
+ * @head: header
+ *
+ * Description: blabla
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *pim;
+
+	if (head == NULL)
+		return;
+
+	while (*head)
+	{
+		pim = (*head)->next;
+		free(head);
+		*head = pim;
+	}
+
+	head = NULL;
+}
