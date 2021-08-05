88022db64a9a13437c43404099b3c420f0484854
root <3290@holbertonschool.com>
2021-08-05 12:02:03 -0700

    0fin

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,13 +2,13 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
+ *print_binary - print in binary
+ *@n: number 
  * Return: Always 0.
  */
 void print_binary(unsigned long int n)
- {
-	 if (n > 1)
-		 print_binary(n >> 1);
-	 _putchar((n & 1) + '0');
- }
+{
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
+}
