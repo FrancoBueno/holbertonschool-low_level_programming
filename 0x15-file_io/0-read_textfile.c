dfa46d435519e310bcd07fc2835eb0d028dc5e1b
root <3290@holbertonschool.com>
2021-08-09 10:35:15 -0700

    primero

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ *
+ *
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t n, n2, n3;
+	char *buffersize;
+
+	if (filename == NULL)
+		return (0);
+	buffersize = malloc(sizeof(char)*letters);
+	if(buffersize == NULL)
+		return(0);
+	
+	n = open(filename,O_RDONLY);
+	n2 = read(n, buffersize,letters);
+	n3 = write(STDOUT_FILENO, buffersize, n2);			
+	
+	if (n == -1 || n2 == -1 || n3 == -1 || n3 != n2)
+	{
+		free(buffersize);
+		return (0);
+	}
+	free(buffersize);
+	close (n);
+	return (n3);
+}
