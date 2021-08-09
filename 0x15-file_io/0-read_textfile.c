741ce59abccdf4f0d3e965d1bbe675acafcd8605
root <3290@holbertonschool.com>
2021-08-09 10:52:49 -0700

    finish 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,9 @@
 #include "main.h"
 /**
- *
- *
+ *read_textfile - read the text of the file selected
+ *@filename: name of file
+ *@letters: characters to print
+ *Return: n3 asdas
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -10,20 +12,20 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 		return (0);
-	buffersize = malloc(sizeof(char)*letters);
-	if(buffersize == NULL)
-		return(0);
-	
-	n = open(filename,O_RDONLY);
-	n2 = read(n, buffersize,letters);
-	n3 = write(STDOUT_FILENO, buffersize, n2);			
-	
+	buffersize = malloc(sizeof(char) * letters);
+	if (buffersize == NULL)
+		return (0);
+
+	n = open(filename, O_RDONLY);
+	n2 = read(n, buffersize, letters);
+	n3 = write(STDOUT_FILENO, buffersize, n2);
+
 	if (n == -1 || n2 == -1 || n3 == -1 || n3 != n2)
 	{
 		free(buffersize);
 		return (0);
 	}
 	free(buffersize);
-	close (n);
+	close(n);
 	return (n3);
 }
