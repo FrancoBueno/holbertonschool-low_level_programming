56e8a486cce4da4ef7ed02c84616eb32e0010a39
root <3290@holbertonschool.com>
2021-08-10 06:34:34 -0700

    fix 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -25,7 +25,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!text_content)
 		return (-1);
 	
-	fl = open(filename, O_WRONLY, O_APPEND);
+	fl = open(filename, O_WRONLY | O_APPEND);
 
 	if (fl == -1)
 		return (-1);
