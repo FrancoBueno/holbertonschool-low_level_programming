3c400c58bc9093a232e2c6acd73ab2f765e341f5
root <3290@holbertonschool.com>
2021-08-10 06:41:45 -0700

    2app

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,8 +1,9 @@
 #include "main.h"
 #include <string.h>
 /**
- *
- *
+ *_strlen - string lenghta asdkjajskd
+ *@str: asdasdas
+ *Return: retusandasnda
  */
 size_t _strlen(char *str)
 {
@@ -13,7 +14,10 @@ size_t _strlen(char *str)
 	return (pos);
 }
 /**
- *
+ *append_text_to_file - asdasdas
+ *@filename: name of file asdka
+ *@text_content: content of the file letters
+ *Return: append to tesaxasfakfsao
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
@@ -24,7 +28,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	if (!text_content)
 		return (-1);
-	
+
 	fl = open(filename, O_WRONLY | O_APPEND);
 
 	if (fl == -1)
