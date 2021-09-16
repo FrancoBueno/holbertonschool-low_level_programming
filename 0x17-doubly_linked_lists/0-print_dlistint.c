2b0e370e87a5453d052d2a80ed2479bb3453d630
root <3290@holbertonschool.com>
2021-09-16 08:56:39 -0700

    fix

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,17 +1,19 @@
 #include "lists.h"
 #include <stdio.h>
 /**
- *
+ *print_dlistint - Raorkaokrsa
+ *@h: header
+ *Return: Successss
  */
 
 size_t print_dlistint(const dlistint_t *h)
 {
-  int i = 0;
-  while(h != NULL)
-    {
-      printf("%d\n", h->n);
-      h = h->next;
-      i++;
-    }
-  return (i);
+int i = 0;
+while (h != NULL)
+{
+printf("%d\n", h->n);
+h = h->next;
+i++;
+}
+return (i);
 }
