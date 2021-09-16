80d7df9625ed9b4d38c9b4d8e86222324337d7c5
root <3290@holbertonschool.com>
2021-09-16 11:07:16 -0700

    4

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,12 +1,14 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
- *
+ *free_dlistint - ASDASDOa
+ *@head: heder
+ *Return:suceess
  */
 
 void free_dlistint(dlistint_t *head)
 {
-if(head)
+if (head)
 {
 free_dlistint(head->next);
 free(head);
