e0b6dffd45224c5e6b9cdda76c82cbc029665cdf
FrancoBuenoHolberton <3290@holbertonschool.com>
2021-09-16 15:37:46 +0000

    0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ *
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+  int i = 0;
+
+  while(h != NULL)
+    {
+      printf("%d\n", h->n);
+      h = h->next;
+      i++;
+    }
+  return (i);
+}
