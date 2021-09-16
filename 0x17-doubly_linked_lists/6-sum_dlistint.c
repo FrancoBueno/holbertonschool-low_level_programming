5475421321150600d22dd27ecc9792ab4ce85ebd
root <3290@holbertonschool.com>
2021-09-16 12:02:25 -0700

    63242

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ *
+ */
+int sum_dlistint(dlistint_t *head)
+{
+  int suma = 0;
+
+if (!head)
+  return (0);
+while (head)
+{
+suma += head->n;
+head = head->next;
+}
+return (suma);
+}
