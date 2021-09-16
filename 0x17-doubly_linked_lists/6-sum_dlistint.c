ac805458254fe7dba65c508b3b2a66ad947cf038
root <3290@holbertonschool.com>
2021-09-16 12:04:04 -0700

    beti

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,14 +1,16 @@
 #include "lists.h"
 #include <stdlib.h>
 /**
- *
+ *sum_dlistint - Adoadsoaso
+ *@head: asodaosdaos
+ *Return:Asadaosdoasd
  */
 int sum_dlistint(dlistint_t *head)
 {
-  int suma = 0;
+int suma = 0;
 
 if (!head)
-  return (0);
+return (0);
 while (head)
 {
 suma += head->n;
