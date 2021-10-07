0c91d1caa2a0d9aa145b611fae0b74651d16a1fd
root <3290@holbertonschool.com>
2021-10-07 15:29:19 -0700

    3faltabetty

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,65 @@
+#include "hash_tables.h"
+
+/**
+ * 
+ *
+ */
+
+hash_node_t *hash_new_node(const char *key, const char *value)
+{
+	hash_node_t *node = NULL;
+
+	node = malloc(sizeof(hash_node_t));
+	if (!node)
+		return(NULL);
+	node->key = strdup(key);
+	if (node->key == NULL)
+	{
+		free(node);
+		return (NULL);
+	}
+	node->value = strdup(value);
+	if (!node->value)
+	{
+		free(node->key);
+		free(node);
+		return(NULL);
+	}
+	node->next = NULL;
+	return (node);
+}
+/**
+ *
+ *
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *hash_node, *tmp;
+	char *valuenew;
+
+	if (ht == NULL || ht->size == 0 || ht->array == NULL || key == NULL || strlen(key) == 0 || value == NULL)
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[index];
+	while (tmp != NULL)
+	{
+		if(strcmp(tmp->key, key) == 0)
+		{
+			valuenew = strdup(value);
+			if (valuenew == NULL)
+				return (0);
+			free(tmp->value);
+			tmp->value = valuenew;
+			return (1);
+		}
+		tmp = tmp->next;
+	}
+	hash_node= hash_new_node(key, value);
+	if (hash_node == NULL)
+		return (0);
+	hash_node->next = ht->array[index];
+	ht->array[index] = hash_node;
+	return (1);
+}
