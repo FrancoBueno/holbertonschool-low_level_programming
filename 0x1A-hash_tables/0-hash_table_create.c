39cf062d363572f57a3538af1a5b35d66acf7758
root <3290@holbertonschool.com>
2021-10-08 07:09:32 -0700

    v

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -19,7 +19,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hasht->size = size;
 	hasht->array = malloc(size * sizeof(hash_node_t *));
 	if(hasht->array == NULL)
-		return (NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 		hasht->array[i] = NULL;
 	return (hasht);
