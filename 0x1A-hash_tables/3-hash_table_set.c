0c0fe9505f5798dd9d759ce29c69b1fcac056cc6
root <3290@holbertonschool.com>
2021-10-08 07:24:36 -0700

    fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,4 +1,34 @@
 #include "hash_tables.h"
+/**
+ *hash_new_node - Function that make new node
+ *@key: keyy
+ *@value: valueid
+ *Return: New node
+ */
+
+hash_node_t *hash_new_node(const char *key, const char *value)
+{
+	hash_node_t *node = NULL;
+
+	node = malloc(sizeof(hash_node_t));
+	if (!node)
+		return (NULL);
+	node->key = strdup(key);
+	if (node->key == NULL)
+	{
+		free(node);
+		return (NULL);
+	}
+	node->value = strdup(value);
+	if (!node->value)
+	{
+		free(node->key);
+		free(node);
+		return (NULL);
+	}
+	node->next = NULL;
+	return (node);
+}
 
 /**
  *hash_table_set - Function that add elemnts to the hash table
