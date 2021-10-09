b381e95678c6d2762b0afa0219655defeefef602
root <3290@holbertonschool.com>
2021-10-08 19:35:57 -0700

    0x1A. C - Hash tables

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -3,7 +3,29 @@
 /**
  *
  */
-void hash_table_delete(hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht)
 {
+	hash_node_t *newnode = NULL;
+	unsigned long int a;
 
+	if (ht)
+	{
+		for (a = 0; a < ht->size; a++)
+		{
+			if (ht->array[a])
+			{
+				while (ht->array[a])
+				{
+					newnode = ht->array[a]->next;
+					free(ht->array[a]->key);
+					free(ht->array[a]->value);
+					free(ht->array[a]);
+					ht->array[a] = newnode;
+				}
+				free(ht->array[a]);
+			}
+		}
+		free(ht);
+		free(ht->array);
+	}
 }
