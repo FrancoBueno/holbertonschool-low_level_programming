4cb13f500166f8fe326ac7090a72dcedc73cba96
root <3290@holbertonschool.com>
2021-11-25 11:32:45 -0800

    fin

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python3
+"""
+    Perimetro de la isla 
+"""
+
 def island_perimeter(grid):
+    perimeter = 0
     """island perimeter"""
-    for i in len(grid):
-        for j in len(len())
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                if i == 0 or grid[i-1][j] == 0:
+                    perimeter += 1
+                if j == 0 or grid[i][j-1] == 0:
+                    perimeter += 1
+                if len(grid)-1 or grid[i+1][j] == 0:
+                    perimeter += 1
+                if j == len(grid[i])-1 or grid[i][j+1] == 0:
+                    perimeter += 1
+    return perimeter
