fcd8b519c1f15c0567c651615202264877a40a79
root <3290@holbertonschool.com>
2022-01-18 07:54:09 -0800

    header

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef SEARCH_H
+#define SEARCH_H
+#include <stdio.h>
+
+/**
+ * Header of the excercise
+ */
+
+/*Prototype*/
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+#endif
